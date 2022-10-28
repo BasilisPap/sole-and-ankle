be0f7ada65ac60cbfd466571d075717b2ffb5ef8
Vasilis Papadopoulos <basilis.papadopoulos@gmail.com>
2022-10-28 11:38:43 -0500

    Exercise 2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,9 +29,15 @@ const Header = () => {
 const MainHeader = styled.div`
   padding: 0 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: baseline;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 48px;
+  margin: 0 auto;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
@@ -39,6 +45,7 @@ const NavLink = styled.a`
   text-decoration: none;
   color: ${COLORS.gray[900]};
   font-weight: ${WEIGHTS.medium};
+  padding: 24px 0;
 
   &:first-of-type {
     color: ${COLORS.secondary};
