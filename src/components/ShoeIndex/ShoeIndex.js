4290ad1ccf23dd464b8e8a9871a048bf8e8d1cd6
Vasilis Papadopoulos <basilis.papadopoulos@gmail.com>
2022-10-28 11:54:16 -0500

    Exercise 3

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -25,7 +25,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
           </Select>
         </Header>
         <Spacer size={34} />
-        <ShoeGrid />
+        {/* <ShoeGrid /> */}
       </MainColumn>
       <LeftColumn>
         <Breadcrumbs>
@@ -42,17 +42,32 @@ const ShoeIndex = ({ sortId, setSortId }) => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+display: flex;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+/* background-color: rgba(0,255,0,.4); */
+flex-basis: 280px;
+order: 1;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+/* background-color: rgba(255,0,0,.4); */
+flex: 1;
+order: 2;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+display: flex;
+align-items: center;
+
+`;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+  flex: 1;
 `;
 
 export default ShoeIndex;
